a09d2ada8e944bf70fbeef87264699a66338d854
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-03-25 19:05:39 +0530

    fixed linting issue

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,34 +1,31 @@
 return {
   "mfussenegger/nvim-lint",
-  event = {
-    "BufReadPre",
-    "BufNewFile"
-  },
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
   config = function()
-    local lint = require("lint");
+    local lint = require("lint")
 
-    lint.linter_by_ft = {
+    lint.linters_by_ft = {
       javascript = { "eslint_d" },
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
       python = { "pylint" },
-
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.api.nvim_create_amd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
       end,
-
     })
 
     vim.keymap.set("n", "<leader>l", function()
       lint.try_lint()
-    end
-    )
-  end
+    end, { desc = "Trigger linting for current file" })
+  end,
 }
+
