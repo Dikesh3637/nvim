c8ff3593178e83ee67bdfdd74149a08f0986a019
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-04-14 20:14:49 +0530

    add save keymap

--- init.lua
+++ init.lua
@@ -5,6 +5,13 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
 
+-- Normal mode mapping
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+
+-- Insert mode mapping
+vim.api.nvim_set_keymap('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
