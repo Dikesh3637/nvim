2ecbb19c8f8289ce6934994c1161ad1f9e4fca5d
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-04-21 15:34:10 +0530

    added goto-preview and trouble

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,18 +5,23 @@ return {
     tag = "0.1.6",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      local actions = require("telescope.actions")
+      local trouble = require("trouble.providers.telescope")
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
       -- This is your opts table
-      local actions = require("telescope.actions")
       require("telescope").setup({
         defaults = {
           mappings = {
             i = {
               ["<C-k>"] = actions.move_selection_previous,
               ["<C-j>"] = actions.move_selection_next,
-
+              ["<c-t>"] = trouble.open_with_trouble
+            },
+            n = {
+              ["<c-t>"] = trouble.open_with_trouble
             }
           }
         },
