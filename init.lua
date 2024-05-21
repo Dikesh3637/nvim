b29c37b17b32451c5e3f2aaa5169c3ea80d688f3
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-05-21 21:53:17 +0530

    added the ability to copy

--- init.lua
+++ init.lua
@@ -12,6 +12,8 @@ vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true 
 -- Insert mode mapping
 vim.api.nvim_set_keymap('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
 
+--copying support
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
