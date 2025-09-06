8be384c1e9b9bf61d9ecc8872fb1134697147a5f
Dikesh3637 <dikeshreddy.dev@gmail.com>
2025-09-06 17:14:39 +0530

    theme change and add tmux-nvim-navigator

--- init.lua
+++ init.lua
@@ -695,8 +695,6 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				gopls = {},
-				pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
@@ -793,9 +791,7 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
-				go = { "gofumpt" },
 				-- Conform can also run multiple formatters sequentially
-				python = { "isort", "black" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,97 +4,97 @@
 -- See the kickstart.nvim README for more information
 return {
 	-- catpuccin themes
-	-- {
-	--   {
-	--     'catppuccin/nvim',
-	--     name = 'catppuccin',
-	--     priority = 1000,
-	--     config = function()
-	--       require('catppuccin').setup {
-	--         flavour = 'mocha', -- latte, frappe, macchiato, mocha
-	--         background = { -- :h background
-	--           light = 'latte',
-	--           dark = 'mocha',
-	--         },
-	--         transparent_background = true, -- disables setting the background color.
-	--         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-	--         term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-	--         dim_inactive = {
-	--           enabled = false, -- dims the background color of inactive window
-	--           shade = 'dark',
-	--           percentage = 0.15, -- percentage of the shade to apply to the inactive window
-	--         },
-	--         no_italic = false, -- Force no italic
-	--         no_bold = false, -- Force no bold
-	--         no_underline = false, -- Force no underline
-	--         styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-	--           comments = { 'italic' }, -- Change the style of comments
-	--           conditionals = { 'italic' },
-	--           loops = {},
-	--           functions = {},
-	--           keywords = {},
-	--           strings = {},
-	--           variables = {},
-	--           numbers = {},
-	--           booleans = {},
-	--           properties = {},
-	--           types = {},
-	--           operators = {},
-	--           -- miscs = {}, -- Uncomment to turn off hard-coded styles
-	--         },
-	--         color_overrides = {},
-	--         custom_highlights = {},
-	--         default_integrations = true,
-	--         integrations = {
-	--           cmp = true,
-	--           gitsigns = true,
-	--           nvimtree = true,
-	--           treesitter = true,
-	--           notify = false,
-	--           mini = {
-	--             enabled = true,
-	--             indentscope_color = '',
-	--           },
-	--           -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-	--         },
-	--       }
-	--       vim.cmd.colorscheme 'catppuccin-mocha'
-	--     end,
-	--   },
-	-- },
-	--gruvbox.nvim
 	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = function()
-			-- Default options:
-			require("gruvbox").setup({
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = true,
-					emphasis = true,
-					comments = true,
-					operators = false,
-					folds = true,
-				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				invert_intend_guides = false,
-				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = true,
-			})
-			vim.cmd("colorscheme gruvbox")
-		end,
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			config = function()
+				require("catppuccin").setup({
+					flavour = "mocha", -- latte, frappe, macchiato, mocha
+					background = { -- :h background
+						light = "latte",
+						dark = "mocha",
+					},
+					transparent_background = true, -- disables setting the background color.
+					show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+					term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+					dim_inactive = {
+						enabled = false, -- dims the background color of inactive window
+						shade = "dark",
+						percentage = 0.15, -- percentage of the shade to apply to the inactive window
+					},
+					no_italic = false, -- Force no italic
+					no_bold = false, -- Force no bold
+					no_underline = false, -- Force no underline
+					styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+						comments = { "italic" }, -- Change the style of comments
+						conditionals = { "italic" },
+						loops = {},
+						functions = {},
+						keywords = {},
+						strings = {},
+						variables = {},
+						numbers = {},
+						booleans = {},
+						properties = {},
+						types = {},
+						operators = {},
+						-- miscs = {}, -- Uncomment to turn off hard-coded styles
+					},
+					color_overrides = {},
+					custom_highlights = {},
+					default_integrations = true,
+					integrations = {
+						cmp = true,
+						gitsigns = true,
+						nvimtree = true,
+						treesitter = true,
+						notify = false,
+						mini = {
+							enabled = true,
+							indentscope_color = "",
+						},
+						-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+					},
+				})
+				vim.cmd.colorscheme("catppuccin-mocha")
+			end,
+		},
 	},
+	--gruvbox.nvim
+	-- {
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		-- Default options:
+	-- 		require("gruvbox").setup({
+	-- 			terminal_colors = true, -- add neovim terminal colors
+	-- 			undercurl = true,
+	-- 			underline = true,
+	-- 			bold = true,
+	-- 			italic = {
+	-- 				strings = true,
+	-- 				emphasis = true,
+	-- 				comments = true,
+	-- 				operators = false,
+	-- 				folds = true,
+	-- 			},
+	-- 			strikethrough = true,
+	-- 			invert_selection = false,
+	-- 			invert_signs = false,
+	-- 			invert_tabline = false,
+	-- 			invert_intend_guides = false,
+	-- 			inverse = true, -- invert background for search, diffs, statuslines and errors
+	-- 			contrast = "", -- can be "hard", "soft" or empty string
+	-- 			palette_overrides = {},
+	-- 			overrides = {},
+	-- 			dim_inactive = false,
+	-- 			transparent_mode = true,
+	-- 		})
+	-- 		vim.cmd("colorscheme gruvbox")
+	-- 	end,
+	-- },
 	--oil nvim
 	{
 		{
@@ -126,8 +126,8 @@ return {
 	-- codeium ai completion
 	-- {
 	--   'Exafunction/codeium.nvim',
-	--   dependencies = {
 	--     'nvim-lua/plenary.nvim',
+	--   dependencies = {
 	--     'hrsh7th/nvim-cmp',
 	--   },
 	--   config = function()
@@ -208,69 +208,69 @@ return {
 	},
 
 	--avante.nvim
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		lazy = false,
-		version = false, -- Never set this value to "*"! Never!
-		opts = {
-			-- add any opts here
-			-- for example
-			provider = "gemini",
-			-- cursor_applying_provider = 'groq',
-			-- behaviour = {
-			-- 	--- ... existing behaviours
-			-- 	enable_cursor_planning_mode = true, -- enable cursor planning mode!
-			-- },
-			gemini = {
-				model = "gemini-2.0-flash",
-				timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-				temperature = 0,
-				--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-			},
-		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		build = "make",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
-			"echasnovski/mini.pick", -- for file_selector provider mini.pick
-			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-			"ibhagwan/fzf-lua", -- for file_selector provider fzf
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"zbirenbaum/copilot.lua", -- for providers='copilot'
-			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
-			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
-		},
-	},
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	event = "VeryLazy",
+	-- 	lazy = false,
+	-- 	version = false, -- Never set this value to "*"! Never!
+	-- 	opts = {
+	-- 		-- add any opts here
+	-- 		-- for example
+	-- 		provider = "gemini",
+	-- 		-- cursor_applying_provider = 'groq',
+	-- 		-- behaviour = {
+	-- 		-- 	--- ... existing behaviours
+	-- 		-- 	enable_cursor_planning_mode = true, -- enable cursor planning mode!
+	-- 		-- },
+	-- 		gemini = {
+	-- 			model = "gemini-2.0-flash",
+	-- 			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+	-- 			temperature = 0,
+	-- 			--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+	-- 		},
+	-- 	},
+	-- 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	-- 	build = "make",
+	-- 	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"stevearc/dressing.nvim",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		--- The below dependencies are optional,
+	-- 		"echasnovski/mini.pick", -- for file_selector provider mini.pick
+	-- 		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+	-- 		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+	-- 		"ibhagwan/fzf-lua", -- for file_selector provider fzf
+	-- 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+	-- 		"zbirenbaum/copilot.lua", -- for providers='copilot'
+	-- 		{
+	-- 			-- support for image pasting
+	-- 			"HakonHarnes/img-clip.nvim",
+	-- 			event = "VeryLazy",
+	-- 			opts = {
+	-- 				-- recommended settings
+	-- 				default = {
+	-- 					embed_image_as_base64 = false,
+	-- 					prompt_for_file_name = false,
+	-- 					drag_and_drop = {
+	-- 						insert_mode = true,
+	-- 					},
+	-- 					-- required for Windows users
+	-- 					use_absolute_path = true,
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 		{
+	-- 			-- Make sure to set this up properly if you have lazy=true
+	-- 			"MeanderingProgrammer/render-markdown.nvim",
+	-- 			opts = {
+	-- 				file_types = { "markdown", "Avante" },
+	-- 			},
+	-- 			ft = { "markdown", "Avante" },
+	-- 		},
+	-- 	},
+	-- },
 
 	--nvim-ts-autotag
 	{
@@ -290,5 +290,25 @@ return {
 		config = function()
 			vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
 		end,
+	},
+
+	-- vim-tmux-navigator.nvim
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
 	},
 }
